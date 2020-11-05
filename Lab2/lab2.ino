36f4ebc7812f21a88d609ce091c22864e1d54df7
Anna Kravchuk <54149858+AnnaKravchuk12@users.noreply.github.com>
2020-11-05 23:35:34 +0200

    Add file lab2

---
+++ Lab2/lab2.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+const int bus = 4;     
+OneWire oneWire(bus);
+
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  delay(5000);
+}
